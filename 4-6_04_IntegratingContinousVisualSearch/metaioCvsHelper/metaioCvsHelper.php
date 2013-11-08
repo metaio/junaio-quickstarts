f88f706e36380ce7e6af2463c1f669a3edc4e757
manep <mane.puaca@metaio.com>
2013-11-08 13:08:13 +0100

    When asking user for password, emphasized that it should be original (and not md5)

--- 4-6_04_IntegratingContinousVisualSearch/metaioCvsHelper/metaioCvsHelper.php
+++ 4-6_04_IntegratingContinousVisualSearch/metaioCvsHelper/metaioCvsHelper.php
@@ -430,7 +430,7 @@ function getEMail()
 
 function getPassword()
 {
-    echo PHP_EOL."Please enter your password!".PHP_EOL;
+    echo PHP_EOL."Please enter your ORIGINAL password! (metaioCvsHelper will md5 it for you)".PHP_EOL;
     $password = trim(fgets(STDIN));
     return $password;
 }
