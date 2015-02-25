9e2af1154b0aed162fa8db97c4d32abd3b9406bf
manep <mane.puaca@metaio.com>
2015-02-25 09:46:32 +0100

    Removed error reporting.

--- LBS_10_ExcelBasedLBS/index.php
+++ LBS_10_ExcelBasedLBS/index.php
@@ -1,7 +1,5 @@
 <?php
 
-error_reporting(E_ALL);
-ini_set('display_errors', '1');
 require_once 'jsonTranslator.php';
 require_once '../ARELLibrary/arel_object.class.php';
 require_once '../ARELLibrary/arel_xmlhelper.class.php';
